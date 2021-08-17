a7f2cf196619750f30581cb6dab790337c3f52da
prasadksc <prasadksc123@gmail.com>
2021-08-17 19:59:02 +0530

    first commit

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,7 +11,7 @@ const NavBar = ({value,onChange}) => {
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/"><img src={img} className="navbar_logo" alt="wall frame logo" /></Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
